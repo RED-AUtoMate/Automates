8449110acd70802504aa02b05adc65863ed43519
imadhou <imadhou00@gmail.com>
2021-02-03 23:30:24 +0100

    test

---
+++ aa.sh
@@ -0,0 +1,7 @@
+echo "# Automates" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/RED-AUtoMate/Automates.git
+git push -u origin main

094d4d77d09c7bc25853d9e6cf9adbcd9b793dd1
bapn3454 <73539312+bapn3454@users.noreply.github.com>
2021-02-20 16:53:11 +0100

    structure vu avec mourtada

--- src/main/java/EtatsCompose.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class EtatsCompose extends AbstractEtats{
-    private List<String> nom;
-
-    public EtatsCompose(List<String> nom) {
-        this.nom = nom;
-    }
-}

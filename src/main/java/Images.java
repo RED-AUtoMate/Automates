013537bd332917ef2176a836f80f9d1a0dfd49c2
rayani00 <rayanhaddai1@gmail.com>
2021-02-21 17:06:11 +0100

    A faire : generation du fichier .gv, et regler le probléme de la derniére ligne manquante

--- src/main/java/Images.java
+++ src/main/java/Images.java
@@ -17,13 +17,13 @@ public class Images {
                 "[[1, c], [3, a], [3, b]]\n" +
                 "2\n" +
                 "[[2, a], [3, a]]\n";
-        char transition[][] = new char[200][3];
-
+        char transition[][] = new char[30][3];
+        int g = 0;
         try {
             automateJson = automateJson.replace("]]\n", "]]\n\n").replace("[[", "[").replace("]]", "]");
             String chaineTransitions[] = automateJson.split("\n\n");
             for (int j = 0; j < chaineTransitions.length; j++) {
-                // Initialiser le nombre de trasition pour un etat a 0
+                // Initialiser le nombre de transitions pour un etat a 0
                 int nbTrans = 0;
                 // Calcul du nombre de transitions pour chaque etat
                 for (int i = 0; i < chaineTransitions[j].length(); i++) {
@@ -32,12 +32,13 @@ public class Images {
                     }
                 }
                 chaineTransitions[j] = chaineTransitions[j].replace("[", "").replace("]", "").replace(",", "").replaceAll("\\s", "").trim();
-                int b = 0;
+                // Pour avancer dans la chaine de caractere
                 int c = 0;
+                // Recuperer le premier element qui est la source et le supprimer juste apres
                 char d = chaineTransitions[j].charAt(0);
                 chaineTransitions[j] = chaineTransitions[j].substring(1);
                 // Remplissage du tableau transitions
-                for (int k = b; k < k + nbTrans; k++) {
+                for (int k = g; k < g + nbTrans; k++) {
                     for (int h = 0; h < 3; h++) {
                         if (h == 0) {
                             transition[k][h] = d;
@@ -46,9 +47,9 @@ public class Images {
                             c++;
                         }
                     }
-                    b = k;
                 }
-
+                g = g + nbTrans;
+                c = 0;
             }
             BufferedWriter b = new BufferedWriter(new FileWriter("/home/rayani00/test.gv"));
             System.out.println("Fichier cree avec succes");

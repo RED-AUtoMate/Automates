02dc43826293a9fa8fe070a2bbceb82df67d9def
imadhou <imadhou00@gmail.com>
2021-02-22 01:18:14 +0100

    creation d'automates aleatoirement Checked

--- src/main/java/JsonDeal.java
+++ src/main/java/JsonDeal.java
@@ -1,30 +1,77 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.Random;
 
 public class JsonDeal {
 
-    public void generer_aleatoirement(int nb_automates, int nb_etats, String[] alphabet, int nb_transition, int max_config_par_etat){
+
+    public void automate_to_json(Automates automate){
+        List<String> alphabet = automate.getAlphabet();
+        List<Transitions> transitions = automate.getTransitions();
+
+    }
+
+
+    /*
+    Creation d'un auomate aleatoirement :
+        nb_etats : le nombre d'etat que l'automate va contenir
+        nb_transitions : le nombre total de transition
+        alphabet : les non termineaux
+
+        la fonction va renvoyer un objet de la classe Automate
+     */
+    public void random_aut(int nb_etats, int nb_transition, String[] alphabet) {
+        Random random = new Random();
+        /* L'ensemble d'etats de l'automate */
+        ArrayList etats = new ArrayList();
+        for (int i = 1; i <= nb_etats; i++) {
+            etats.add(String.valueOf(i));
+        }
+
+        /* L'etat de depart et l'etat final */
+        String etat_depart = String.valueOf(random.nextInt(nb_etats));
+        String etat_final = String.valueOf(random.nextInt(nb_etats));
+
+        /* les transitions */
+        HashMap transitions = new HashMap();
+        for (int j = 0; j < nb_transition; j++) {
+            String depart = String.valueOf(random.nextInt(nb_etats));
+            String destination = String.valueOf(random.nextInt(nb_etats));
+            String mot = alphabet[random.nextInt(alphabet.length)];
+            ArrayList<String> transition = new ArrayList<String>();
+            transition.add(0, destination);
+            transition.add(1, mot);
+            if (transitions.containsKey(depart)) {
+                ArrayList arrayList = (ArrayList) transitions.get(depart);
+                arrayList.add(transition);
+                transitions.put(depart, arrayList);
+            } else {
+                ArrayList config = new ArrayList();
+                config.add(transition);
+                transitions.put(depart, config);
+            }
+        }
+    }
+
+    public void generer_aleatoirement(int nb_automates, int nb_etats, String[] alphabet, int nb_transition, int max_config_par_etat) {
         JSONObject jsonAutomates = new JSONObject();
         ArrayList arrayList00 = new ArrayList();
         Random random = new Random();
-        for (int i = 0; i < 1; i++){
-            for (int j = 0; j < nb_transition; j++){
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; j < nb_transition; j++) {
                 ArrayList transitions = new ArrayList();
-                int a = random.nextInt(max_config_par_etat+1);
-                if (a != 0){
+                int a = random.nextInt(max_config_par_etat + 1);
+                if (a != 0) {
                     ArrayList arrayList = new ArrayList();
-                    for (int k = 0; k < a; k++ ){
+                    for (int k = 0; k < a; k++) {
                         int b = random.nextInt(alphabet.length);
                         int c = random.nextInt(nb_etats);
                         ArrayList config = new ArrayList();
@@ -34,22 +81,22 @@ public class JsonDeal {
                     }
                     transitions.add(a);
                     transitions.add(arrayList);
-                }else {
+                } else {
                 }
-                if (transitions.size() != 0){
+                if (transitions.size() != 0) {
                     arrayList00.add(transitions);
                 }
             }
             System.out.println(arrayList00);
             HashMap hashMap = new HashMap();
-            for (int j =0; j < arrayList00.size(); j++){
+            for (int j = 0; j < arrayList00.size(); j++) {
                 ArrayList a = (ArrayList) arrayList00.get(j);
                 System.out.println(a.get(0));
-                if (hashMap.containsKey(a)){
+                if (hashMap.containsKey(a)) {
                     ArrayList ab = (ArrayList) hashMap.get(a);
 
                 }
-                hashMap.put(a.get(0),a.get(1));
+                hashMap.put(a.get(0), a.get(1));
             }
             System.out.println(hashMap);
         }
@@ -58,7 +105,9 @@ public class JsonDeal {
 
     public static void main(String[] args) {
 
+
         JsonDeal jsonDeal = new JsonDeal();
+        jsonDeal.random_aut(4, 8, new String[]{"a", "b", "c"});
         //jsonDeal.generer_aleatoirement(5, 4, new String[]{"a", "b", "c"}, 6, 3);
         JSONParser jsonParser = new JSONParser();
 
@@ -69,8 +118,8 @@ public class JsonDeal {
             /* obtention du contenu du fichier json (definition de l'automate) */
             JSONArray alphabet = (JSONArray) ob.get("Alphabet");
             JSONArray etats = (JSONArray) ob.get("Etats");
-            String init =  ob.get("Init").toString();
-            String fin =  ob.get("Fin").toString();
+            String init = ob.get("Init").toString();
+            String fin = ob.get("Fin").toString();
             JSONArray transitions = (JSONArray) ob.get("Transitions");
 
             /* construction du tableau des differentes transitions */
@@ -78,8 +127,8 @@ public class JsonDeal {
             ArrayList<Transitions> tr = new ArrayList<Transitions>();
 
             /* pour chaque transition on obtien l'etat initiale et les differentes configurations */
-            for (int i = 0; i < transitions.size(); i++){
-                JSONArray transition =(JSONArray) transitions.get(i);
+            for (int i = 0; i < transitions.size(); i++) {
+                JSONArray transition = (JSONArray) transitions.get(i);
 
 
                 /* obtention de l'etat source */
@@ -89,12 +138,12 @@ public class JsonDeal {
                 JSONArray trans = (JSONArray) transition.get(1);
 
                 /* chaque trnsition sera representée dans un tableau arrayList2 */
-                ArrayList arrayList2 = new ArrayList();
+                ArrayList<ArrayList<String>> arrayList2 = new ArrayList<ArrayList<String>>();
 
                 /* pour chaque configuration on obtient le mot de l'alphabet et l'etat destination */
-                for (int j = 0; j < trans.size(); j++){
+                for (int j = 0; j < trans.size(); j++) {
                     ArrayList arrayList1 = (ArrayList) trans.get(j);
-                    ArrayList arrayList = new ArrayList();
+                    ArrayList<String> arrayList = new ArrayList<String>();
 
                     /* chaque configuration est representée par arraylist1 0=>e_dest, 1=>mot de l alphabet*/
                     arrayList.add(arrayList1.get(0).toString());
@@ -105,17 +154,12 @@ public class JsonDeal {
                     arrayList2.add(arrayList);
                 }
 
-
-                System.out.println(arrayList2);
-
                 /* instantiation d'une transition et attribution des valeurs à ses attributs*/
 
 
 
                 /* ajout de la transition dans l'ensemble des transitions de notre automate */
             }
-
-
 
 
         } catch (IOException e) {

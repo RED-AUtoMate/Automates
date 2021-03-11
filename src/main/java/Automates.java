00dc784203f4c403a02eca55b5ee18213be64f79
bapn3454 <73539312+bapn3454@users.noreply.github.com>
2021-03-11 22:15:55 +0100

    determinisation en cours

--- src/main/java/Automates.java
+++ src/main/java/Automates.java
@@ -74,7 +74,7 @@ public class Automates {
     // ALGORITHMES
 
     public void determiniser(){
-
+        // contient les lignes du tableau de determinaisation ( on l'initialise avec l'etat de depart )
         Queue<List> file_etat_cree = new LinkedList<List>();
         List debut = new ArrayList();
         debut.add(this.getEtatDepart().getNom());
@@ -87,14 +87,21 @@ public class Automates {
             Etats etat = new Etats();
             // concatener les etats groupés : à définir
             etat.setNom(etats_preced.toString());
+            // contient les colonnes du tableau
             HashMap<String,List> hash = new HashMap<String,List>();
 
             for(int i =0;i<etats_preced.size();i++){
+                // on pourrait declarer l ici
+                // ICI on pourrait faire un parcours des successeurs de etats_preced pour les ajouter à hash
+                // on pourrait meme remplacer la boucle suivante
+                // sans oublier de remplacer les alphabet.get/size par les vriables adéquates
                 for (int j=0;j<this.alphabet.size();j++){
-                    //verifier cette ligne
+                    //verifier cette ligne la condition est inutile : on ajoute tous les etats sans distinction
+                    // on devrait plutot verifier si ils ont alphabet.get(j) comme successeur
                     if (hash.containsKey(this.alphabet.get(j))){
                         // l'idée est là
                         List l = (List)hash.get(this.alphabet.get(j));
+                        // pourquoi etats_preced alors qu'en bas on met un get(i)???
                         l.add(etats_preced);
                         hash.put(this.alphabet.get(j),l);
                     } else {
@@ -107,14 +114,17 @@ public class Automates {
             }
             System.out.println(hash);
             // à revoir
+            // le fait que c'est à l'exterieur de la boucle peut poser des problemes de reinnitialisation
             ArrayList hashToList = new ArrayList();
             for(String key: hash.keySet()){
                 file_etat_cree.add(hash.get(key));
                 ArrayList config = new ArrayList();
+                // je pense qu'ici on devrait ajouter la key au lieu de get(key) PAS SÛR
                 config.add(0,hash.get(key).toString());
-               config.add(1,hash.get(key));
-               hashToList.add(config);
-               etat.setTransitions(hashToList);
+//                config.add(1,hash.get(key));
+                config.add(1,key);
+                hashToList.add(config);
+                etat.setTransitions(hashToList);
             }
         }
 
